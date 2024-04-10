1d189f1d73b234a97babd130614ffc4cb257104c
Cesar David Lopez Lara <cesar.lopez@qrsof.com>
2024-04-10 10:00:59 -0600

    init project

---
+++ src/main/java/com/cesar/springboot/app/SpringbootCursoApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/springboot/app/SpringbootCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
